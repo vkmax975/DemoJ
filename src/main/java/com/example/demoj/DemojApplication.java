73fe3206708360584bac7153d50401974319dcd6
k <k@DESKTOP-VVMN49I>
2023-11-28 00:34:10 +0530

    demo1

---
+++ src/main/java/com/example/demoj/DemojApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoj/DemojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
